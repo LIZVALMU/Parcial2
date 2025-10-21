a3c064c2ba715540cd49db8766dc3a6cf03a2005
alison valderrama <alisvalderrama@gmail.com>
2025-10-21 10:11:55 -0500

    Secuencia de Luca

---
+++ lucas/src/main/java/escuelaing/edu/parcial/controller/ControllerSecuencia.java
@@ -0,0 +1,17 @@
+package escuelaing.edu.parcial.controller;
+
+import org.springframework.annotation.RestController;
+import org.springframework.annotation.Autowired;
+import
+
+@RestController
+public class ControllerSecuencia{
+    
+    @Autowired
+    private SecuenciaLuca secuenciaLuca;
+
+    @GetMapping
+    public 
+    
+
+}
